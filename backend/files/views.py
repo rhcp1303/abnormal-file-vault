7fba14fd76422e271559e9064cbed8357b7956c3
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-03-30 11:44:46 +0530

    adding api to calculate saved file size

--- backend/files/views.py
+++ backend/files/views.py
@@ -3,11 +3,47 @@ from rest_framework.response import Response
 from .models import File
 from .serializers import FileSerializer
 from .services import calculate_file_hash
+from django.db.models import Sum
+from django.db.models import Sum, F, Q, Count, IntegerField
+from rest_framework.decorators import action
 
 
 class FileViewSet(viewsets.ModelViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
+
+    @action(detail=False, methods=['get'])
+    def storage_statistics(self, request):
+        """
+        Calculates and returns storage statistics.
+        """
+        unique_storage = File.objects.filter(related_file__isnull=True).aggregate(total_size=Sum('size'))[
+                             'total_size'] or 0
+        total_storage_if_duplicates = File.objects.all().aggregate(total_size=Sum('size'))['total_size'] or 0
+        referenced_counts = File.objects.filter(
+            id__in=File.objects.filter(related_file__isnull=False).values_list('related_file', flat=True)
+        ).annotate(
+            reference_count=Count('references')
+        ).values('id', 'reference_count')
+
+        storage_savings = 0
+        referenced_file_ids = [item['id'] for item in referenced_counts]
+
+        if referenced_file_ids:
+            referenced_file_sizes = File.objects.filter(id__in=referenced_file_ids).values('id', 'size')
+            size_map = {item['id']: item['size'] for item in referenced_file_sizes}
+
+            for ref_info in referenced_counts:
+                file_id = ref_info['id']
+                count = ref_info['reference_count']
+                size = size_map.get(file_id, 0)
+                storage_savings += size * count
+
+        return Response({
+            'unique_storage_used': unique_storage,
+            'total_storage_if_duplicates': total_storage_if_duplicates,
+            'storage_savings': storage_savings,
+        }, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
         file_obj = request.FILES.get('file')
